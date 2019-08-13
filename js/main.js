342f4fe026c3079fb5c2bff69913885cacdf3f83
berto <asandoval.hatchet@gmail.com>
2019-08-12 21:01:35 -0700

    day one content

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function myFunk() {
+    let question = prompt('Tell me your name');
+    console.log('Your name is ' + question);
+}
+
+function myHobbies() {
+    let onclick = console.log('My favorite hobby is whittling.');
+}
