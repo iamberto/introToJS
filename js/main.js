ddc0d7c39f27cb7b565cedb5846f5e8370e196b5
berto <asandoval.hatchet@gmail.com>
2019-08-14 20:18:42 -0700

    takeHome-dayTwo-assignment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var firstName = 'Berto';
-var age = 28;
 
 //Old Way
 // console.log('My name is ' + firstName + ' and my age is ' + age);
@@ -22,3 +20,23 @@ var age = 28;
 // const topThreeMovies = ['Batman Begins', ' The Dark Knight', ' and The Dark Knight Rises'];
 // console.log(`My top three movies are ${topThreeMovies}.`);
 
+
+
+// Asks for the user's first name
+let firstName = prompt('What is your name?');
+// Asks for the user's last name
+let lastName = prompt('What is your last name?');
+// Logs the user's first name to the console
+console.log(firstName);
+// Alerts the user's last name
+alert(lastName);
+// *Asks for the user's birthday
+let birthday = prompt('What is your birthday?');
+// confirms whether the birthday recieved is correct
+let verifyDate = confirm(`Is your birthday ${birthday}?`)
+// alerts the resulting birthday
+alert(birthday);
+
+// Merge your takehome-day2 branch with gh-pages so I can test it
+// Send me the url of the gh-pages on Slack
+// * Extra points if you use JavaScript's Date object for the birthday.
