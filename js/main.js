35989057a20b5e3fb252800713d95f61e3d761e4
berto <asandoval.hatchet@gmail.com>
2019-08-14 19:53:13 -0700

    dayTwo in class work

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var firstName = 'Berto';
+var age = 28;
+
+//Old Way
+// console.log('My name is ' + firstName + ' and my age is ' + age);
+
+//Better way (use backticks, not quote marks)
+// console.log(`My name is ${firstName} and my age is ${age}.`);
+
+// alert(`My name is ${firstName} and my age is ${age}.`);
+
+// const birthday = '07/26/1991';
+
+// let fixDate = new Date(birthday);
+// console.log(fixDate);
+
+// let hello = new String('Sup, fam!');
+// alert(hello);
+
+// console.log(firstName.length);
+
+// const topThreeMovies = ['Batman Begins', ' The Dark Knight', ' and The Dark Knight Rises'];
+// console.log(`My top three movies are ${topThreeMovies}.`);
+
