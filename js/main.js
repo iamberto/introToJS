2f34aca2a687ae2819e3aa67859589d9818e2830
berto <asandoval.hatchet@gmail.com>
2019-08-21 16:46:17 -0700

    stuck on this last Try-At-Home

--- js/main.js
+++ js/main.js
@@ -27,6 +27,40 @@
 
 //Oh shiiiiiiit. I did it!!
 
+// Asks if the user is over 18
+
+
+// If the user is over 18, allow them to proceed, otherwise 
+// alert the user that they are not old enough.
+
+const ofAge = confirm("Are you over 18?");
+
+if (ofAge == true){
+    alert("Great! Let's keep going.")
+} else if (ofAge != true){
+    alert('Bye, Felicia. Not old enough.');
+} 
+// Now that all your users at this point are at least 18,
+// ask if they are less than 80 years old.
+ const underOldAge = confirm("Now, are you UNDER 80?");
+// Now ask the users who are between 18 and 80 if 
+// they like Star Wars. 
+// If they are over 80, ask if they like prunes.
+if (ofAge == true && underOldAge == true){
+    confirm("So...You like Stah Wahs?")
+} else if (underOldAge != true){
+    confirm("Do you like prunes?");
+}
+
+// If they say they don't Star Wars, end the program. In all other 
+// cases, alert them with a message.
+// if (ageAnswer == false){
+//     alert("Ok. We're done.")
+// } else {
+//     alert("Woop, woop!!");
+// }
+
+
 
 
 
