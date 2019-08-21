644c4c3bc26139ef9dfdb06dbf11399c53eb9229
berto <asandoval.hatchet@gmail.com>
2019-08-21 15:10:08 -0700

    in-class part 2 done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 // const firstVariable = "Ay fam";
 // const secondVariable = new String ("Yo dawg");
 
-const firstVariable = 1;
-const secondVariable = new String ("1");
+// const firstVariable = 1;
+// const secondVariable = new String ("1");
 
 // Use the logical operators to determine 
 // if they are strictly equal.
 // Now use the logical operators to determine 
 // if they are loosely equal.
 
-if (firstVariable == secondVariable){
-    console.log(`Yuuuh, ${firstVariable} and ${secondVariable} are hella equal.`);
-} else {
-    console.log(`No way...Not eva. ${firstVariable} and ${secondVariable} will never be equal.`);
-} if (firstVariable != secondVariable) {
-    console.log(`It's a sad fact of reality that the greetings ${firstVariable} and ${secondVariable} will never be equal.`);
-}
+// if (firstVariable == secondVariable){
+//     console.log(`Yuuuh, ${firstVariable} and ${secondVariable} are hella equal.`);
+// } else {
+//     console.log(`No way...Not eva. ${firstVariable} and ${secondVariable} will never be equal.`);
+// } if (firstVariable != secondVariable) {
+//     console.log(`It's a sad fact of reality that the greetings ${firstVariable} and ${secondVariable} will never be equal.`);
+// }
+
+// const numberOne = prompt('Pick a Number');
+// const numberTwo = prompt('Okay, now pick another number, pls.');
+
+// //turn the strings into integers
+// let parseEmBaybee = (parseInt(numberOne, 10) + parseInt(numberTwo, 10));
+
+// alert(parseEmBaybee);
+
+//Oh shiiiiiiit. I did it!!
+
+
+
 
