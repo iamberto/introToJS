18b913614b480eb045eb433f1e92edba09e09e93
berto <asandoval.hatchet@gmail.com>
2019-08-25 22:09:08 -0700

    day3stillInProgress

--- js/main.js
+++ js/main.js
@@ -61,86 +61,86 @@
 //.sort sorts your array alphabetically
 
 
-// question = prompt('How old are you?');
+question = prompt('How old are you?');
 
-// let returnValue = parseInt(question);
+let returnValue = parseInt(question);
 
-// if(returnValue >= 18){
-//     alert("You're old enough! Hooray!!");
-// } else {
-//     alert("Naw, sorry fam. C-ya.")
-// };
+if(returnValue >= 18){
+    alert("You're old enough! Hooray!!");
+} else {
+    alert("Naw, sorry fam. C-ya.")
+};
 
-// if(returnValue <= 80){
+if(returnValue <= 80){
     
-// }
+}
 
 //5 arrays
 
-let arrayOne = [
-    'red',
-    'firetruck'
-];
+// let arrayOne = [
+//     'red',
+//     'firetruck'
+// ];
 
-let arrayTwo = [
-    'blue',
-    'blueberries'
-];
+// let arrayTwo = [
+//     'blue',
+//     'blueberries'
+// ];
 
-let finalArray = [
-    arrayOne,
-    arrayTwo
-];
+// let finalArray = [
+//     arrayOne,
+//     arrayTwo
+// ];
 
-console.log(finalArray[0][1]);
+// console.log(finalArray[0][1]);
 
-// In your scripts.js file, create 5 arrays, each with one of your 
-// top 5 movies' title and the main character's name
+// // In your scripts.js file, create 5 arrays, each with one of your 
+// // top 5 movies' title and the main character's name
 
 
 
-let movieArrayOne = [
-    "The Dark Knight",
-    "Bruce Wayne"
-];
+// let movieArrayOne = [
+//     "The Dark Knight",
+//     "Bruce Wayne"
+// ];
 
-let movieArrayTwo = [
-    "Watchmen",
-    "Owlman"
-];
+// let movieArrayTwo = [
+//     "Watchmen",
+//     "Owlman"
+// ];
 
-let movieArrayThree = [
-    "Lincoln",
-    "Abraham Lincoln"
-];
+// let movieArrayThree = [
+//     "Lincoln",
+//     "Abraham Lincoln"
+// ];
 
-let movieArrayFour = [
-    "Reservoir Dogs",
-    "Mr. Orange"
-];
+// let movieArrayFour = [
+//     "Reservoir Dogs",
+//     "Mr. Orange"
+// ];
 
-let movieArrayFive = [
-    "Kill Bill",
-    "Beatrix Kiddo"
-];
+// let movieArrayFive = [
+//     "Kill Bill",
+//     "Beatrix Kiddo"
+// ];
 
-// Add all of these arrays to a final array named favoriteMovies
+// // Add all of these arrays to a final array named favoriteMovies
 
-let favoriteMovies = [
-    movieArrayOne,
-    movieArrayTwo,
-    movieArrayThree,
-    movieArrayFour,
-    movieArrayFive
-];
+// let favoriteMovies = [
+//     movieArrayOne,
+//     movieArrayTwo,
+//     movieArrayThree,
+//     movieArrayFour,
+//     movieArrayFive
+// ];
 
-alert(favoriteMovies);
+// alert(favoriteMovies);
 
-// Create an array with just the titles of your favorite movies and alert it
+// // Create an array with just the titles of your favorite movies and alert it
 
-let movieTitle = [
+// let movieTitle = [
 
-]
+// ]
 
 // Create another array with your favorite and least favorite of the top 5 
 // and console.log it.
@@ -148,3 +148,77 @@ let movieTitle = [
 
 // Check the length of your favoriteMovies array by alerting it.
 
+
+
+
+//LOOPS
+
+//while loop
+// let x = 0;
+// while (x <= 10) {
+//     console.log(x);
+//     x++;
+// }
+
+//for Loop
+
+// for (let i = 0; i < 18; i++) {
+//     if (i == 4){
+//         continue;
+//     }
+//     console.log(i);
+
+// }
+
+// for (let i = 15; i > 0; i--) {
+//     console.log(i);
+// }
+
+// let i = 0;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i<10);
+
+// let arrayThree = [1, 2, 3, 4, 5];
+
+// arrayThree.forEach(function(x){
+//     console.log(`I am ${x}`);
+
+// })
+
+// let myString = "Hi, my name is WHAT";
+// for (i of myString){
+//     console.log(i);
+// }
+
+// let people = [{
+//     firstName : 'Berto',
+//     lastName : 'Sandoval',
+//     age : 28
+// }, {
+//     firstName : 'Not',
+//     lastName : 'Berto',
+//     age : 33
+// }];
+
+// for (person of people){
+//     console.log(person.firstName)
+// } 
+
+// for (person in people) {
+//     console.log(people[person]);
+// }
+
+
+// let users = [
+//     {
+//      username: 'unicorn88',
+//      createdAt: new Date()
+//  },
+//  {
+//      username: 'rino44',
+//      createdAt: new Date()
+//  }
+// ]
