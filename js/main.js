dab99085d44098cf8e69d23c9e9aae783178918d
berto <asandoval.hatchet@gmail.com>
2019-08-27 22:53:04 -0700

    Day 5 Takehome gh-pages

--- js/main.js
+++ js/main.js
@@ -1,42 +1,50 @@
+// Day 5 Takehome
 
-//Old Way
-// console.log('My name is ' + firstName + ' and my age is ' + age);
+const greeting = document.getElementById('greeting');
+const groceryList = document.getElementById('groceryList');
+// const groceryItem = document.getElementById('groceryItem');
+const getGroceryItem = document.getElementById('getGroceryItem');
 
-//Better way (use backticks, not quote marks)
-// console.log(`My name is ${firstName} and my age is ${age}.`);
+let arrayOfGroceryItems = [];
 
-// alert(`My name is ${firstName} and my age is ${age}.`);
-
-// const birthday = '07/26/1991';
-
-// let fixDate = new Date(birthday);
-// console.log(fixDate);
-
-// let hello = new String('Sup, fam!');
-// alert(hello);
-
-// console.log(firstName.length);
-
-// const topThreeMovies = ['Batman Begins', ' The Dark Knight', ' and The Dark Knight Rises'];
-// console.log(`My top three movies are ${topThreeMovies}.`);
+greeting.innerHTML = 'Berto\'s Grocery List';
 
 
+// function addGroceryItem(arr, value) {
+//     arr.push(value);
+// }
 
-// Asks for the user's first name
-let firstName = prompt('What is your name?');
-// Asks for the user's last name
-let lastName = prompt('What is your last name?');
-// Logs the user's first name to the console
-console.log(firstName);
-// Alerts the user's last name
-alert(lastName);
-// *Asks for the user's birthday
-let birthday = prompt('What is your birthday?');
-// confirms whether the birthday recieved is correct
-let verifyDate = confirm(`Is your birthday ${birthday}?`)
-// alerts the resulting birthday
-alert(birthday);
 
-// Merge your takehome-day2 branch with gh-pages so I can test it
-// Send me the url of the gh-pages on Slack
-// * Extra points if you use JavaScript's Date object for the birthday.
+// function ejectGroceryItemToList(values) { 
+//     //goal push array items to list on html
+
+//     if (arrayOfGroceryItems !== undefined) {
+//         for (gcheckout rocery = 0; grocery < arrayOfGroceryItems.length; grocery++) {
+//             let li = document.getElementById(`groceryItem${grocery + 1}`);
+//             li.innerHTML = values[grocery];
+//         }
+//     }
+//     else {
+//         alert('Sorry, the array is empty right now.');
+//     }
+// }
+
+
+// // console.log(arrayOfGroceryItems);
+// ejectGroceryItemToList(arrayOfGroceryItems);
+function addToList(){
+    if(getGroceryItem.value == ""){
+        console.log('Nuthin');
+    } else{
+    console.log('Sumthin')
+    arrayOfGroceryItems.push(getGroceryItem.value);
+    for (grocery = 0; grocery < arrayOfGroceryItems.length; grocery++){
+        let li = document.getElementById(`groceryItem${grocery + 1}`);
+        li.innerHTML = `# ${grocery + 1} ${arrayOfGroceryItems[grocery]}`;
+        getGroceryItem.value = '';
+    }
+    console.log(arrayOfGroceryItems);
+};
+
+}
+
