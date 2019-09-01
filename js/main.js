fca222c13a5b571220abf5fbee69a29956c87347
berto <asandoval.hatchet@gmail.com>
2019-09-01 13:38:16 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+// Day 6 Takehome
+
+// const greeting = document.getElementById('greeting');
+// const groceryList = document.getElementById('groceryList');
+// // const groceryItem = document.getElementById('groceryItem');
+// const getGroceryItem = document.getElementById('getGroceryItem');
+
+// let arrayOfGroceryItems = [];
+
+// greeting.innerHTML = 'Berto\'s Grocery List';
+
+
+// function addGroceryItem(arr, value) {
+//     arr.push(value);
+// }
+
+
+// function ejectGroceryItemToList(values) { 
+//     //goal push array items to list on html
+
+//     if (arrayOfGroceryItems !== undefined) {
+//         for (grocery = 0; grocery < arrayOfGroceryItems.length; grocery++) {
+//             let li = document.getElementById(`groceryItem${grocery + 1}`);
+//             li.innerHTML = values[grocery];
+//         }
+//     }
+//     else {
+//         alert('Sorry, the array is empty right now.');
+//     }
+// }
+
+
+// // console.log(arrayOfGroceryItems);
+// ejectGroceryItemToList(arrayOfGroceryItems);
+// function addToList(){
+//     if(getGroceryItem.value == ""){
+//         console.log('Nuthin');
+//     } else{
+//     console.log('Sumthin')
+//     arrayOfGroceryItems.push(getGroceryItem.value);
+//     for (grocery = 0; grocery < arrayOfGroceryItems.length; grocery++){
+//         let li = document.getElementById(`groceryItem${grocery + 1}`);
+//         li.innerHTML = `# ${grocery + 1} ${arrayOfGroceryItems[grocery]}`;
+//         getGroceryItem.value = '';
+//     }
+//     console.log(arrayOfGroceryItems);
+// };
+
+// }
+
