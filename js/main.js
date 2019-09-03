5de7c112bbfe36457b642504dbf4c53d84796c2a
berto <asandoval.hatchet@gmail.com>
2019-09-02 17:37:42 -0700

    Fahrenheit to Celcius and vice-versa

--- js/main.js
+++ js/main.js
@@ -1,51 +1,39 @@
-
 // Day 6 Takehome
 
-// const greeting = document.getElementById('greeting');
-// const groceryList = document.getElementById('groceryList');
-// // const groceryItem = document.getElementById('groceryItem');
-// const getGroceryItem = document.getElementById('getGroceryItem');
+let valueInput = document.getElementById('valueInput');
+let changeToFahrenheit = document.getElementById('changeToFahrenheit');
+let changeToCelcius = document.getElementById('changeToCelcius');
+let valueOutput = document.getElementById('valueOutput');
 
-// let arrayOfGroceryItems = [];
+changeToCelcius.addEventListener('click', () => {
+    let checkNumber = isNaN(valueInput.value);
+    if (!checkNumber) {
+        console.log('Yee, fam. That is a number.');
+        changeToCelcius.value = `${parseFloat((valueInput.value - 32) * 5 / 9).toFixed(2)}`;
+        console.log(changeToCelcius.value);
+        valueOutput.innerHTML = `${changeToCelcius.value}`;
+    } else {
+        console.log('Bruh, not a number!');
+    };
 
-// greeting.innerHTML = 'Berto\'s Grocery List';
+});
 
+changeToFahrenheit.addEventListener('click', () => {
+    let checkNumber = isNaN(valueInput.value);
+    if (!checkNumber) {
+        console.log('Yee, fam. That is a number.');
+        changeToFahrenheit.value = `${parseFloat((valueInput.value * 9 / 5) + 32).toFixed(2)}`;
+        console.log(changeToFahrenheit.value);
+        valueOutput.innerHTML = `${changeToFahrenheit.value}`;
 
-// function addGroceryItem(arr, value) {
-//     arr.push(value);
-// }
+    } else {
+        console.log('Bruh, not a number!');
+    };
+});
 
-
-// function ejectGroceryItemToList(values) { 
-//     //goal push array items to list on html
-
-//     if (arrayOfGroceryItems !== undefined) {
-//         for (grocery = 0; grocery < arrayOfGroceryItems.length; grocery++) {
-//             let li = document.getElementById(`groceryItem${grocery + 1}`);
-//             li.innerHTML = values[grocery];
-//         }
-//     }
-//     else {
-//         alert('Sorry, the array is empty right now.');
-//     }
-// }
-
-
-// // console.log(arrayOfGroceryItems);
-// ejectGroceryItemToList(arrayOfGroceryItems);
-// function addToList(){
-//     if(getGroceryItem.value == ""){
-//         console.log('Nuthin');
-//     } else{
-//     console.log('Sumthin')
-//     arrayOfGroceryItems.push(getGroceryItem.value);
-//     for (grocery = 0; grocery < arrayOfGroceryItems.length; grocery++){
-//         let li = document.getElementById(`groceryItem${grocery + 1}`);
-//         li.innerHTML = `# ${grocery + 1} ${arrayOfGroceryItems[grocery]}`;
-//         getGroceryItem.value = '';
-//     }
-//     console.log(arrayOfGroceryItems);
-// };
-
-// }
-
+// Add Event Listener to h3 id
+valueOutput.addEventListener('click', () => {
+    let valueOutputToCelcius = `${changeToCelcius.valueOutput.value)}`;
+    console.log(valueOutputToCelcius);
+}
+);
