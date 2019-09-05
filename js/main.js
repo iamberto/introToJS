fcabfe1fbe7df11d9581f90b81d65f0164e4f6e9
berto <asandoval.hatchet@gmail.com>
2019-09-04 17:57:26 -0700

    didn't finish takehome part

--- js/main.js
+++ js/main.js
@@ -1,8 +1,67 @@
-function myFunk() {
-    let question = prompt('Tell me your name');
-    console.log('Your name is ' + question);
+let h1 = document.querySelector(`h1`);
+
+const container = document.querySelector('.container');
+console.log(container);
+
+function globalListItems() {
+    // create ul/ol with classes, attributes needed, create li with classes, attributes, or IDs needed 
+
+    //create ul
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    //atributes/classes/id
+    li.className = 'list-group-item list-group-item-info';
+    li.innerText = 'I am a list item.';
+    li.id = 'mySpecialListItem';
+
+    const image = document.createElement('img');
+    image.setAttribute('alt', 'This is an empty image container.');
+    console.log(image);
+
+    //Append to container->ul->li
+    ul.appendChild(li);
+
+    container.appendChild(ul);
+    
 }
 
-function myHobbies() {
-    let onclick = console.log('My favorite hobby is whittling.');
+globalListItems();
+
+function createDynamicCard() {
+    //create Elements needed for card
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'panel panel-default text-center col-md-6';
+
+    const h1 = document.createElement('h1');
+    h1.className = 'panel-title text-center';
+    h1.innerHTML = 'This is the title';
+
+    const image = document.createElement('img');
+    image.className = 'col-md-4 img-responsive';
+
+    const p = document.createElement('p');
+    p.innerText = 'Exhale, expel, Recast my tale, Read my allegorical elegy';
+
+    const icon = document.createElement('i'); 
+
+    console.log('this is the div: ', cardDiv);
+
+    //Append cardDiv to container
+
+    container.appendChild(cardDiv);
+    //Append child tags to cardDiv div
+    cardDiv.appendChild(h1);
+    cardDiv.appendChild(image);
+    cardDiv.appendChild(p);
+    cardDiv.appendChild(icon);
+
+    //add classes, IDs, and attributes
+
+
+
+
+
+
 }
+
+createDynamicCard()
